068f852157410c7bd783541f5626aa080ccae6b8
Guykeny <guykeny.ndayizeye8@gmail.com>
2026-06-17 12:38:59 +0200

    feat: ajout de fichier makefile

--- simeis-main/README.md
+++
@@ -1,3 +0,0 @@
-# Simeis
-
-Jeu par API
--- simeis-main/simeis-data/src/ship/shipstats.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Clone, Serialize, Default)]
-pub struct ShipStats {
-    pub speed: f64,
-    pub fuel_consumption: f64,
-    pub hull_usage_rate: f64,
-}
